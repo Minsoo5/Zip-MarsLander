67a8e8a5ae542f422812143a6a52b10fbd76064f
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-08-27 16:19:10 -0400

    floating around with no fuel

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -8,12 +8,12 @@ public class OnBoardComputer implements BurnStream {
         //a = (speed * speed)/(2 * altitude)
         int burn = (int) ((currentSpeed * currentSpeed) / (2 * currentAltitude));
 
-        if (currentAltitude <= 100 && currentSpeed < 2) {
-            burn = 98;
+        if (currentAltitude == 0 && currentSpeed < 1) {
+            burn = -1;
         }
 
         System.out.println(burn); /*hack!*/
-        return burn;
+        return burn + 100;
     }
 
 }
--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -88,18 +88,20 @@ public class Vehicle {
     public DescentEvent getStatus(int tick) {
         // create and return a new DescentEvent object
         int report = -100;
+        //Currently only spitting out -100 or 0
+//        if (this.Altitude > 1) {
+//            if (this.Velocity >= 10) {
+//                report = -3;
+//            } else if (this.Velocity < 10 && this.Velocity >= 3) {
+//                report = -2;
+//            } else if (this.Velocity < 3 && this.Velocity >= 1) {
+//                report = 0;
+//            }
+//        }
+//        if (this.Fuel == 0) {report = -1;}
 
-        if (this.Altitude > 1) {
-            if (this.Velocity >= 10) {
-                report = -3;
-            } else if (this.Velocity < 10 && this.Velocity >= 3) {
-                report = -2;
-            } else if (this.Velocity < 3 && this.Velocity >= 1) {
-                report = 0;
-            }
-        }
-        if (this.Fuel == 0) {report = -1;}
-        DescentEvent de = new DescentEvent(tick, this.Velocity, this.Fuel, this.Altitude, report);
+
+        DescentEvent de = new DescentEvent(tick, this.Velocity, this.Fuel, this.Altitude, Flying);
         // filled in with the state of the vehicle.
         //Need the current status of the vehicle
 
